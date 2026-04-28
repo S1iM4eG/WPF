51278bd2548d1e4f1f844276adf6ef3d37834d41
S1iM <S1iM@HOME-PC>
2026-04-28 21:21:20 +0400

    ReusableMenuBar DONE

---
+++ MenuBar/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MenuBar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
