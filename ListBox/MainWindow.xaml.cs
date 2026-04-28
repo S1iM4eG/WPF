01433fc8229f5ccbfa4b1fd083cc8bed5d9822b3
S1iM <S1iM@HOME-PC>
2026-04-28 18:49:34 +0400

    ListBox

---
+++ ListBox/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void button_Click(object sender, EventArgs e)
+        {
+            switch((sender as Button).Content)
+            {
+                case "ADD":
+                    if (!ListBox.Items.Contains(tbInput.Text))
+                    {
+                        if (tbInput.Text.Trim() == "") break;
+                        ListBox.Items.Add(tbInput.Text);
+                        tbInput.Text = "";
+                        tbInput.Focus();
+                    }
+                    break;
+                case "DEL":
+                    if (ListBox.SelectedIndex >= 0)
+                        ListBox.Items.RemoveAt(ListBox.SelectedIndex);
+                    break;
+                case "CLR": ListBox.Items.Clear(); 
+                    break;
+            }
+        }
+
+
+        private void TbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) button_Click(btnAdd, null);
+        }
+    }
+}
