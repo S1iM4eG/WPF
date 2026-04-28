45ff5fa5b22eab5941b58f5ce4cfccd37df040c2
S1iM <S1iM@HOME-PC>
2026-04-28 22:30:58 +0400

    ClearableTextBox in progress

---
+++ CustomTextBoxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomTextBoxControl.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        string placeholder;
+        public string Placeholder
+        {
+            get => placeholder;
+            set => placeholder = tbPlaceholder.Text = value;
+        }
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e) => txtInput.Text = "";
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e) => tbPlaceholder.Visibility = txtInput.Text == "" ? Visibility.Visible : Visibility.Hidden;
+        }
+}
